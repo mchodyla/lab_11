028290aeb0815ab3e9d259443eff8f4ec6a32c41
mchodyla <mchodyla@gmail.com>
2020-01-04 02:07:26 +0100

    UI update

--- src/lab_package/Main.java
+++ src/lab_package/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("E0_menu.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("LAB");
+        primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
     }
 
