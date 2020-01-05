eeac1905f2c969f59ab5266f6a2f71ca2f446c3d
MarcinZielen <marcin.zielen.96@gmail.com>
2020-01-05 18:09:43 +0100

    Scene changing - fix

---
+++ src/gui/E2_controller.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class E2_controller {
+
+    @FXML
+    public Button b_cancel;
+
+    public void gotoMenu(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("E0_menu.fxml"));
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(new Scene(root, 500, 300));
+        window.show();
+    }
+}
